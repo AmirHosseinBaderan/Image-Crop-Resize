b361f48185e1e3af5042840e343fec552ac0fb07
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-07-15 13:23:24 +0430

    create resize and crop using system drawing

--- ImageCropResize/ImageCropResize/Program.cs
+++ ImageCropResize/ImageCropResize/Program.cs
@@ -1,4 +1,7 @@
 ﻿using static System.Console;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 while (true)
 {
@@ -26,11 +29,53 @@ while (true)
 
     void CropImage()
     {
-       
+        Image image = Bitmap.FromFile(path);
+
+        var sizes = size.Split("/");
+        var width = int.Parse(sizes[0]);
+        var heigth = int.Parse(sizes[0]);
+
+        Rectangle rectangle = new(0, 0, width, heigth);
+        Bitmap cropImage = new(rectangle.Width, rectangle.Height);
+        using Graphics graphics = Graphics.FromImage(cropImage);
+        graphics.DrawImage(image, rectangle, new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+        var extension = Path.GetExtension(path);
+        var savePath = $"F:/Resizes/Resized-{Guid.NewGuid()}{extension}";
+
+        cropImage.Save(savePath);
+
+        WriteLine($"Image Croped and save at {savePath}");
     }
 
     void ResizeImage()
     {
-       
+        Image image = Bitmap.FromFile(path);
+
+        var sizes = size.Split("/");
+        var width = int.Parse(sizes[0]);
+        var heigth = int.Parse(sizes[0]);
+
+        Rectangle destRect = new(0, 0, width, heigth);
+        Bitmap destImage = new(width, heigth);
+        destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+        using Graphics graphics = Graphics.FromImage(destImage);
+        graphics.CompositingMode = CompositingMode.SourceCopy;
+        graphics.CompositingQuality = CompositingQuality.HighQuality;
+        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        graphics.SmoothingMode = SmoothingMode.HighQuality;
+        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+        using ImageAttributes wrapMode = new();
+        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+
+        var extension = Path.GetExtension(path);
+        var savePath = $"F:/Resizes/Resized-{Guid.NewGuid()}{extension}";
+
+        destImage.Save(savePath);
+
+        WriteLine($"Image Resized and save at {savePath}");
     }
 }
